6a5a7c2a858d352ff83e4122d89ed29af0ce548b
songh <35511695+sh9369@users.noreply.github.com>
2018-08-02 14:11:33 +0800

    fix second check

--- project/ip_check_C2.py
+++ project/ip_check_C2.py
@@ -45,7 +45,7 @@ def get_date_flow(es, gte, lte, time_zone, dip):
             "sip": {
                 "terms": {
                     "field": "sip",
-                    "size": 100,
+                    "size": 1000,
                     "order": {
                         "flow": "desc"
                     }
@@ -97,30 +97,31 @@ def calc_MAD(datalist):
 # Second_check: 1）根据dip查找某段时间内所有与其通信的sip在每分钟flow计数；
 #             2）根据flow计数的序列判断是否有异常
 # return: 返回有问题的sip list
-def Second_check(es, gte, lte, time_zone, dip,mylog):
+def Second_check(es, gte, lte, time_zone, dip,mylog,siplist):
     mylog.info('get flow from ES.')
     res = get_date_flow(es=es, gte=gte, lte=lte, time_zone=time_zone, dip=dip)
     ret_siplist = []
     # each sip_item has only one sip but many flows in different time
     for sip_item in res["aggregations"]["sip"]["buckets"]:
-        datelist = []
-        flowlist = []
-        for item in sip_item["date"]["buckets"]:
-            datelist.append(item["key"])
-            flowlist.append(item["flow"]["value"])
-        if len(datelist) < 2:
-            continue
-        mylog.info('*_*_* len of datelist:{} *_*_*'.format(len(datelist)))
-        date_dev = [datelist[i + 1] - datelist[i] for i in range(len(datelist) - 1)]
-        #		print date_dev
-        #		print flowlist
-        mylog.info('*_*_* result of date_dev:{} *_*_*'.format(calc_MAD(date_dev)))
-        mylog.info('*_*_* result of flowlist:{} *_*_*'.format(calc_MAD(flowlist)))
-        # print calc_MAD(date_dev)
-        # print calc_MAD(flowlist)
-        if (calc_MAD(date_dev) <= 60000) and (calc_MAD(flowlist) <= 1):
-            ret_siplist.append(sip_item["key"])
-            mylog.info('*_*_* appending sip:{}. *_*_*'.format(sip_item["key"]))
+        if(sip_item["key"] in siplist):# sip appear in last 5 mins then check 24h
+            datelist = []
+            flowlist = []
+            for item in sip_item["date"]["buckets"]:
+                datelist.append(item["key"])
+                flowlist.append(item["flow"]["value"])
+            if len(datelist) < 3:
+                continue
+            mylog.info('*_*_* len of datelist:{} *_*_*'.format(len(datelist)))
+            date_dev = [datelist[i + 1] - datelist[i] for i in range(len(datelist) - 1)]
+            #		print date_dev
+            #		print flowlist
+            mylog.info('*_*_* result of date_dev:{} *_*_*'.format(calc_MAD(date_dev)))
+            mylog.info('*_*_* result of flowlist:{} *_*_*'.format(calc_MAD(flowlist)))
+
+            # print calc_MAD(flowlist)
+            if (calc_MAD(date_dev) <= 60000) and (calc_MAD(flowlist) <= 1):
+                ret_siplist.append(sip_item["key"])
+                mylog.info('*_*_* appending sip:{}. *_*_*'.format(sip_item["key"]))
     return ret_siplist# sip
 
 class ESclient(object):
@@ -211,41 +212,98 @@ class ESclient(object):
             allrecord[dip]=temp["_source"]
         return allrecord
 
-    def secondcheck(self,gte2,lte,time_zone,dip,mylog):
+    def check_5mins(self,gte1,lte,time_zone,dip,aggs,mylog):
+        search_option = {
+            "size": 0,
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "query_string": {
+                                "query": "dip:{}".format(dip),
+                                'analyze_wildcard': True
+                            }
+                        },
+                        {
+                            "range": {
+                                "@timestamp": {
+                                    "gte": gte1,
+                                    "lte": lte,
+                                    "format": "yyyy-MM-dd HH:mm:ss",
+                                    "time_zone": time_zone
+                                }
+                            }
+                        }
+                    ],
+                    "must_not": []
+                }
+            },
+            "_source": {
+                "excludes": []
+            },
+            "aggs": {
+                "sip": {
+                    "terms": {
+                        "field": "sip",
+                        "size": 1000,
+                        "order": {
+                            "flow": "desc"
+                        }
+                    }
+                }
+            }
+        }
+        search_result = self.__es_client.search(
+            index='tcp-*',
+            body=search_option
+        )
+        allrecord=[]
+        mylog.info('get last 5mins sip.')
+        clean_search_result = search_result['aggregations']["sip"]['buckets']
+        for temp in clean_search_result:
+            #temp is dict
+            allrecord.append(temp['key'])
+        # return sip
+        return allrecord
+
+    def secondcheck(self,gte1,gte2,lte,time_zone,dip,mylog):
         mylog.info('start second check.')
-        return Second_check(self.__es_client, gte2, lte, time_zone, dip,mylog)
+        # check sip，dip in last 5 mins
+        siplis=self.check_5mins(self,gte1,lte,time_zone,dip,mylog)
+        # check sip，dip in last 24h
+        return Second_check(self.__es_client, gte2, lte, time_zone, dip,mylog,siplis)
 
 '''
 checkAlert: 检查alert中关于c&c的info告警，获取dip
 '''
-def checkAlert(index,gte,lte,time_zone,serverNum,dport):
-    querystr={
-        "query":"type:mal_ip",
-        "analyze_wildcard": True
-    }
-    filetype='dip'
-    rangetime={
-        "@timestamp": {
-            "gte": gte,
-            "lte": lte,
-            "format": "yyyy-MM-dd HH:mm:ss",
-            "time_zone":time_zone
-        }
-    }
-    aggs={
-      "date_histogram": {
-        "field": "@timestamp",
-        "interval": "30m",
-        "time_zone": time_zone,
-        "min_doc_count": 1
-      }
-    }
-    #get es list
-    es = ESclient(server =serverNum,port=dport)
-    # mylog.info('connected with es')
-    ip_es_list = es.get_es_ip(index,gte,lte,filetype,time_zone,querystr,rangetime)
-    allalerts=es.es_search_alert(index,gte,lte,filetype,time_zone,querystr,rangetime,aggs)
-    return ip_es_list,es,allalerts
+# def checkAlert(index,gte,lte,time_zone,serverNum,dport):
+#     querystr={
+#         "query":"type:mal_ip",
+#         "analyze_wildcard": True
+#     }
+#     filetype='dip'
+#     rangetime={
+#         "@timestamp": {
+#             "gte": gte,
+#             "lte": lte,
+#             "format": "yyyy-MM-dd HH:mm:ss",
+#             "time_zone":time_zone
+#         }
+#     }
+#     aggs={
+#       "date_histogram": {
+#         "field": "@timestamp",
+#         "interval": "30m",
+#         "time_zone": time_zone,
+#         "min_doc_count": 1
+#       }
+#     }
+#     #get es list
+#     es = ESclient(server =serverNum,port=dport)
+#     # mylog.info('connected with es')
+#     ip_es_list = es.get_es_ip(index,gte,lte,filetype,time_zone,querystr,rangetime)
+#     allalerts=es.es_search_alert(index,gte,lte,filetype,time_zone,querystr,rangetime,aggs)
+#     return ip_es_list,es,allalerts
 
 
 '''
@@ -269,6 +327,7 @@ def searchAndInsert(alerts,ipdict,es,mylog):
     mylog.info('second check insert finished.')
 
 def list_filter(wlis,allwarn):
+    # for Storm suppression
     # wlis is Queue,[{},{},{},...]
     # allwarn is new check records
     # return subwarn which (sip,dip) not in wlis
@@ -320,8 +379,8 @@ def main(startTime,all_IP,serverNum,dport):
     allwarn={}# {dip:[sip,sip,sip...],ip:[],...},
 
     try:
-        for dip in all_IP.keys():# {'dip':[siplist],...}
-            allwarn[dip]=es.secondcheck(gte2,lte,time_zone,dip,mylog)
+        for dip in all_IP.keys():
+            allwarn[dip]=es.secondcheck(gte1,gte2,lte,time_zone,dip,mylog)
     except Exception,e:
         mylog.error('second_check:{}'.format(e))
     # Storm suppression
